fcb02d27beb6fa720d82b8e64d713660c05bf274
Alberto Bennett <abennett1@email.arizona.edu>
2021-10-20 10:23:18 -0700

    homework

--- assignments/07_iupac/iupac.py
+++ assignments/07_iupac/iupac.py
@@ -6,6 +6,7 @@ Purpose: Rock the Casbah
 """
 
 import argparse
+import sys
 
 
 # --------------------------------------------------
@@ -13,14 +14,20 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Sequence translation',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('sequence',
-                        metavar='str',
+                        metavar='SEQ',
+                        nargs='+',
                         help='Input sequence(s)')
 
-    
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
 
     return parser.parse_args()
 
@@ -30,7 +37,37 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    
+    translation = {
+        'A': 'A',
+        'C': 'C',
+        'G': 'G',
+        'T': 'T',
+        'U': 'U',
+        'R': '[AG]',
+        'Y': '[CT]',
+        'S': '[GC]',
+        'W': '[AT]',
+        'K': '[GT]',
+        'M': '[AC]',
+        'B': '[CGT]',
+        'D': '[AGT]',
+        'H': '[ACT]',
+        'V': '[ACG]',
+        'N': '[ACGT]',
+        }
+
+    for seq in args.sequence:
+        regex = ''
+        for base in seq:
+            regex += translation.get(base, '-')
+        print(seq, regex, file=args.outfile)
+        # for base in seq:
+        #     if base in translation:
+        #         print(base, translation.get(base))
+        #     else:
+        #         print(f'Unknown base "{base}"', file=sys.stderr)
+    if args.outfile != sys.stdout:
+        print(f'Done, see output in "{args.outfile.name}"')
 
 
 # --------------------------------------------------

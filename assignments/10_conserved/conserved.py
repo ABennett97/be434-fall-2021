c676cacb44a807b239cd863120086d08f2a866a3
Alberto Bennett <abennett1@email.arizona.edu>
2021-11-09 21:07:59 -0700

    homework

---
+++ assignments/10_conserved/conserved.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Author : abennett1 <abennett1@localhost>
+Date   : 2021-11-09
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import os
+from Bio import SeqIO
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        help='Input file(s)')
+
+    parser.add_argument('-o',
+                        '--outdir',
+                        help='Output directory',
+                        metavar='str',
+                        type=str,
+                        default='split')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    out_dir = args.outdir
+
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    for fh in args.file:
+        parser = SeqIO.parse(fh, 'fasta')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
